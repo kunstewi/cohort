ed30945c5d29cfb51fa7fedfc5820da870df75f1
kunstewi <kan.ai35423@gmail.com>
2024-12-12 22:10:56 +0530

    except string answers

--- week-1/Basic Javascript/stringIndex.js
+++ week-1/Basic Javascript/stringIndex.js
@@ -1,3 +1,8 @@
 // How to get the index of a string ? (index means the position from where the string starts)
 // What if there are multiple same strings and we want the index of the last one ?
 // How to get the elements of a string between two given indexes ?
+
+
+// SOLUTIONS
+
+// 1. How to get the index of a string ? (index means the position from where the string starts)
--- week-1/Basic Javascript/stringLength.js
+++ week-1/Basic Javascript/stringLength.js
@@ -1,1 +1,9 @@
 // How to get the length of a string ?
+
+let string = "Hello World!";
+console.log(string.length); // 12
+
+// OR
+
+let length = string.length;
+console.log(length); // 12
